2e6b8f4f1c11c2c85971de7619cd7006bc69c711
WangSai <1204222505@qq.com>
2019-03-29 00:19:37 +0800

    国际化

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,15 +1,24 @@
+import org.springframework.cglib.core.Local;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.util.Date;
+import java.util.Locale;
 
 public class HelloTest {
     public static void main(String[] args) {
+        /*
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
-        Date date = context.getBean("date", Date.class);
         helloWorld.helloWorld();
         helloWorld.helloWorld2();
-        helloWorld.helloWorldByChinese();
-        System.out.println(date.getTime());
+        helloWorld.helloWorldByChinese();*/
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("messagebeans.xml");
+        String hello = context.getMessage("hello", new String[]{"孙悟空"}, Locale.getDefault(Locale.Category.FORMAT));
+        String now = context.getMessage("now", new Object[]{new Date()}, Locale.getDefault(Locale.Category.FORMAT));
+
+        // 打印条本地化消息
+        System.out.println(hello);
+        System.out.println(now);
     }
 }
