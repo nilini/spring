55fe3f209dcd5fb7cff4bbcb6c3b8100aa86f209
WangSai <1204222505@qq.com>
2019-04-01 15:46:27 +0800

    FieldRetrirvingFactoryBean

---
+++ src/main/java/fieldRetrievingFactory/Son.java
@@ -0,0 +1,9 @@
+package fieldRetrievingFactory;
+
+public class Son {
+    private int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/test/java/fieldRetrievingFactory.java
@@ -0,0 +1,11 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class fieldRetrievingFactory {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("fieldRetrivingFactory.xml");
+        System.out.println("系统获取 theage1的值, " + context.getBean("theAge1"));
+        System.out.println("系统获取 theage2的值, " + context.getBean("theAge2"));
+        System.out.println("Son " + context.getBean("son"));
+    }
+}
