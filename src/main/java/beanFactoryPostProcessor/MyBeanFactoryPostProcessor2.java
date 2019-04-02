f4f554e96052db4717f53d745638003da49e1e83
WangSai <1204222505@qq.com>
2019-04-02 14:09:22 +0800

    配置多个容器后处理器bean

--- src/main/java/beanFactoryPostProcessor/MyBeanFactoryPostProcessor2.java
+++ src/main/java/beanFactoryPostProcessor/MyBeanFactoryPostProcessor2.java
@@ -5,14 +5,14 @@ import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.core.Ordered;
 
-public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor, Ordered {
+public class MyBeanFactoryPostProcessor2 implements BeanFactoryPostProcessor, Ordered {
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        System.out.println("Spring1容器是：" + beanFactory);
+        System.out.println("Spring2容器是：" + beanFactory);
     }
 
     @Override
     public int getOrder() {
-        return 1;
+        return 2;
     }
 }
