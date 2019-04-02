115ca48da3b725c49fc90f9f7b0271d529b1b9b8
saisai-home <saisai-home@sai.com>
2019-04-02 21:43:28 +0800

    ClassPathResource

--- src/test/java/ResourceTest.java
+++ src/test/java/ResourceTest.java
@@ -2,22 +2,34 @@ import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.Iterator;
 import java.util.List;
 
 public class ResourceTest {
     public static void main(String[] args) throws IOException, DocumentException {
         // books.xml位于应用的当前路径下
-        UrlResource url = new UrlResource("file:books.xml");
-        System.out.println(url.getFilename());
-        System.out.println(url.getDescription());
+        UrlResource r = new UrlResource("file:books.xml");
+        //readXml(r);
+
+        //加载类路径下的资源，对于web应用，可自动搜索位于WEB-INF/classes下的资源。
+        ClassPathResource cr = new ClassPathResource("books.xml");
+        // file:/D:/java/project/spring/target/classes/books.xml
+        System.out.println(cr.getURL());
+        readXml(cr);
+
+
+    }
+    public static void readXml(Resource r) throws IOException, DocumentException {
+        System.out.println(r.getFilename());
+        System.out.println(r.getDescription());
 
         SAXReader reader = new SAXReader();
-        Document doc = reader.read(url.getFile());
+        Document doc = reader.read(r.getFile());
 
         Element element = doc.getRootElement();
         List l = element.elements();
