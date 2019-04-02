f7070f3531ff8017deefd4c664b3e5f611410922
WangSai <1204222505@qq.com>
2019-04-02 17:15:24 +0800

    @Resource 与 <property.../>元素的ref效果一样

--- src/main/java/annotation/component/Chinese.java
+++ src/main/java/annotation/component/Chinese.java
@@ -3,14 +3,21 @@ package annotation.component;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+
 // 指定该Bean实例的作用域为prototype
-@Scope("prototpe")
+@Scope("prototype")
 // 指定该类作为Sprign Bean，Bean实例名为chinese
 @Component("chinese")
 public class Chinese {
     private Axe axe;
 
+    @Resource(name="stoneAxe")
     public void setAxe(Axe axe) {
         this.axe = axe;
+    }
+
+    public Axe getAxe() {
+        return axe;
     }
 }
--- src/test/java/compomentTest.java
+++ src/test/java/compomentTest.java
@@ -1,3 +1,4 @@
+import annotation.component.Chinese;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -6,6 +7,8 @@ import java.util.Arrays;
 public class compomentTest {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("component.xml");
-        System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
+//        System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
+        Chinese chinese = context.getBean("chinese", Chinese.class);
+        System.out.println(chinese.getAxe().chop());
     }
 }
